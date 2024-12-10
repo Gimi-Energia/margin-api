dee122ea2d2c734cbc26dfab9a60f46d3a52d79c
fehenriq <lipe.h.r@gmail.com>
2024-12-10 19:06:13 -0300

    :sparkles: feat: add admin models

--- apps/icms/admin.py
+++ apps/icms/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.icms.models import NCM, ICMSRate, NCMGroup, State
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ("name", "code")
+    search_fields = ("name", "code")
+    ordering = ("code",)
+
+
+@admin.register(NCMGroup)
+class NCMGroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "list_ncms")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+    def list_ncms(self, obj):
+        return ", ".join(ncm.code for ncm in obj.ncms.all())
+
+    list_ncms.short_description = "NCMs"
+
+
+@admin.register(NCM)
+class NCMAdmin(admin.ModelAdmin):
+    list_display = ("code", "group")
+    search_fields = ("code", "group__name")
+    ordering = ("code",)
+    list_filter = ("group",)
+
+
+@admin.register(ICMSRate)
+class ICMSRateAdmin(admin.ModelAdmin):
+    list_display = (
+        "state",
+        "group",
+        "internal_rate",
+        "difal_rate",
+        "poverty_rate",
+        "total_rate",
+    )
+    search_fields = ("state__name", "group__name")
+    ordering = ("state", "group")
+    list_filter = ("state", "group")
--- apps/taxes/admin.py
+++ apps/taxes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.taxes.models import Tax
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ("name", "presumed_profit_rate", "real_profit_rate")
+    search_fields = ("name",)
+    ordering = ("name",)
+    list_filter = ("presumed_profit_rate", "real_profit_rate")
